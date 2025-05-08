c3ff5283f46a2eb69e0d5b5cc6055942406d6ead
Han Kwan Saing <saihankwansaing@gmail.com>
2025-05-08 17:58:07 +0900

    made increase btn

--- webdevstudy25/study.js
+++ webdevstudy25/study.js
@@ -1,1 +1,18 @@
-let $showQuantity = 0;
+let count = 0;
+const counterNum = document.querySelector('#counterNum');
+const increaseBtn = document.querySelector('#increaseBtn');
+const resetBtn = document.querySelector('#resetBtn');
+const decreaseBtn = document.querySelector('#decreaseBtn');
+
+increaseBtn.addEventListener('click', function(e){
+    e.preventDefault();   // dont forget to add ()
+    count++;
+    counterNum.textContent = count;
+    if (count > 0) {     // cant use id name in if
+        counterNum.classList.add('greenNum');
+    } else if (count < 0) {
+        counterNum.classList.add('redNum');
+    } else {
+        counterNum.classList.remove;
+    }
+})
