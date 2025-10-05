bc183ca6db27fc667b8fcdceb169a24eaf92487f
Han Kwan Saing <saihankwansaing@gmail.com>
2025-10-05 17:21:13 +0900

    read input

--- javaStudy/basicJava/studyBasic/src/Main.java
+++ javaStudy/basicJava/studyBasic/src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        // first printing out
         System.out.println("hello java");
+
+        // reading the input
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Name: ");
+        String name = scanner.nextLine().trim();
+        System.out.print("Age: ");
+        int age = scanner.nextInt();
+        System.out.println("you are " + name + " and is " + age + " years old");
+
     }
 }
