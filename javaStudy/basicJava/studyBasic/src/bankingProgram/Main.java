22452548bc7bd92678a7da91a01d3e5729d0fa4e
Han Kwan Saing <saihankwansaing@gmail.com>
2025-10-10 16:19:11 +0900

    added showBalance and deposit

--- javaStudy/basicJava/studyBasic/src/bankingProgram/Main.java
+++ javaStudy/basicJava/studyBasic/src/bankingProgram/Main.java
@@ -1,7 +1,56 @@
 package bankingProgram;
 
+import java.util.Scanner;
+
 public class Main {
+
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main (String[] args) {
-        System.out.println("hello from banking file");
+
+        // banking program
+        
+        int choice;
+        int balance = 0;
+        boolean isRunning = true;
+
+        while (isRunning) {
+            System.out.println("Banking program");
+            System.out.println("---------------");
+            System.out.println("1. Check balance");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Exit");
+            System.out.print("Select your desire: ");
+
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1 -> showBalance(balance);
+                case 2 -> balance += deposit();
+                case 3 -> System.out.println("3");
+                case 4 -> isRunning = false;
+                default -> System.out.println("Invalid");
+            }
+        }
     }
+
+        // show balance
+        static void showBalance (int balance) {
+            System.out.println("Your balance is " + balance);
+            System.out.println("********************");
+        }
+
+        // deposit
+        static int deposit () {
+            int amount;
+            System.out.print("Deposit amount: ");
+            amount = scanner.nextInt();
+            if (amount <= 0 ) {
+                System.out.println("Cannot deposit " + amount + " yen");
+                return 0;
+            } else {
+                return amount;
+            }
+        }
 }
