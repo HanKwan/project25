241685052af8427be4ec175387e43af3124ef70c
Han Kwan Saing <saihankwansaing@gmail.com>
2025-10-22 10:48:42 +0900

    created filePath for the musicPlayer

---
+++ javaStudy/basicJava/studyBasic/src/musicPlayer/Main.java
@@ -0,0 +1,12 @@
+package musicPlayer;
+
+import java.io.File;
+
+public class Main {
+
+    public void main(String[] args) {
+
+        String filePath = "src/musicPlayer/d6b2ece0.wav";
+        File file = new File(filePath);
+    }
+}
