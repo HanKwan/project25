12da8b193f8330b93eb03b331a8039f472ad363c
Han Kwan Saing <saihankwansaing@gmail.com>
2025-10-22 15:21:16 +0900

    added err handling

--- javaStudy/basicJava/studyBasic/src/musicPlayer/Main.java
+++ javaStudy/basicJava/studyBasic/src/musicPlayer/Main.java
@@ -1,6 +1,11 @@
 package musicPlayer;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Main {
 
@@ -8,5 +13,15 @@ public class Main {
 
         String filePath = "src/musicPlayer/d6b2ece0.wav";
         File file = new File(filePath);
+
+        try (AudioInputStream audioStream = AudioSystem.getAudioInputStream(file)) {
+            System.out.println("it's fine for now");
+        } catch (UnsupportedAudioFileException e) {
+            System.out.println("Unsupported file");
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (IOException e) {
+            System.out.println("Something is wrong");
+        }
     }
 }
