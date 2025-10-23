2ea8c8fa36c5c8f995eaaa400f84c87a047e4039
Han Kwan Saing <saihankwansaing@gmail.com>
2025-10-23 15:18:58 +0900

    added user's response (musicPLayer done)

--- javaStudy/basicJava/studyBasic/src/musicPlayer/Main.java
+++ javaStudy/basicJava/studyBasic/src/musicPlayer/Main.java
@@ -1,11 +1,10 @@
 package musicPlayer;
 
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.sound.sampled.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
@@ -14,14 +13,44 @@ public class Main {
         String filePath = "src/musicPlayer/d6b2ece0.wav";
         File file = new File(filePath);
 
-        try (AudioInputStream audioStream = AudioSystem.getAudioInputStream(file)) {
-            System.out.println("it's fine for now");
+        try (Scanner scanner = new Scanner(System.in);
+             AudioInputStream audioStream = AudioSystem.getAudioInputStream(file)) {
+
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+
+            String response = "";
+
+            while (!response.equals("q")) {     // not !response = "q"
+
+                System.out.println("Java music player");
+                System.out.println("p for play");
+                System.out.println("s for stop");
+                System.out.println("r for reset");
+                System.out.println("q for quit");
+                System.out.print("Please select: ");
+
+                response = scanner.next().toLowerCase();
+
+                switch (response) {
+                    case "p" -> clip.start();
+                    case "s" -> clip.stop();
+                    case "r" -> clip.setMicrosecondPosition(0);
+                    case "q" -> clip.close();
+                    default -> System.out.println("not valid");
+                }
+            }
+
         } catch (UnsupportedAudioFileException e) {
             System.out.println("Unsupported file");
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
         } catch (IOException e) {
             System.out.println("Something is wrong");
+        } catch (LineUnavailableException e) {
+            System.out.println("line unavailable");
+        } finally {
+            System.out.println("bye");
         }
     }
 }
