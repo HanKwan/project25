58feb49471bc6b59e65ce9534b7ae983a7426223
Han Kwan Saing <saihankwansaing@gmail.com>
2025-10-24 11:06:47 +0900

    added user's guess input

--- javaStudy/basicJava/studyBasic/src/guessThePokemon/Main.java
+++ javaStudy/basicJava/studyBasic/src/guessThePokemon/Main.java
@@ -1,6 +1,7 @@
 package guessThePokemon;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
@@ -10,13 +11,46 @@ public class Main {
 
         ArrayList<Character> wordState = new ArrayList<>();
         int wrongGuess = 0;
+        Scanner scanner = new Scanner(System.in);
 
         for (int i = 0; i < pokemon.length(); i++) {
             wordState.add('_');
         }
 
+        System.out.println("Guess the pokemon");
+        System.out.println("*****************");
+        System.out.println();
         for (char c : wordState) {
             System.out.print(c + " ");
         }
+        System.out.println();
+
+        System.out.print("Guess the pokemon'n name: ");
+        char userGuess = scanner.next().toLowerCase().charAt(0);
+        System.out.println(userGuess);
+    }
+
+    static String health (int wrongGuess) {
+        return switch (wrongGuess) {
+            case 0 -> """
+                     [❤️❤️❤️❤️❤️]
+                     """;
+            case 1 -> """
+                     [❤️❤️❤️❤️❤]
+                     """;
+            case 2 -> """
+                     [❤️❤️❤️❤❤]
+                     """;
+            case 3 -> """
+                     [❤️❤️❤❤❤]
+                     """;
+            case 4 -> """
+                     [❤️❤❤❤❤]
+                     """;
+            case 5 -> """
+                     [❤❤❤❤❤]
+                     """;
+            default -> "";
+        };
     }
 }
