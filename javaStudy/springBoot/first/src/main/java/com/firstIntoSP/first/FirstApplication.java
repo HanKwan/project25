c57bd54f9ecf681e73abf220c84fc971aad569a7
Han Kwan Saing <saihankwansaing@gmail.com>
2025-11-15 11:35:21 +0900

    first hello in spring boot

---
+++ javaStudy/springBoot/first/src/main/java/com/firstIntoSP/first/FirstApplication.java
@@ -0,0 +1,20 @@
+package com.firstIntoSP.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class FirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstApplication.class, args);
+	}
+
+    @GetMapping
+    public String hello() {
+        return "hello spring boot";
+    }
+}
