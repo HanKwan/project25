d1420ff69359f47313bb0424713fcf879fa170e4
Han Kwan Saing <saihankwansaing@gmail.com>
2025-12-21 20:59:10 +0900

    set up the user's input

---
+++ javaStudy/basicJava/studyBasic/src/alarmClock/Main.java
@@ -0,0 +1,25 @@
+package alarmClock;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("H:mm");
+        LocalTime alarmTime = null;
+
+        while (alarmTime == null) {
+            try {
+                System.out.print("set an alarm: ");
+                String inputTime = scanner.nextLine();
+
+                alarmTime = LocalTime.parse(inputTime, formatter);
+                System.out.println(alarmTime);
+            } catch (Exception e) {
+                System.out.println("input TIME");;
+            }
+        }
+    }
+}
