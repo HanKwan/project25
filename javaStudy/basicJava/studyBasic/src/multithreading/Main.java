f4da5c774ed61962e89c8c4e3d24bae88ebfdfa8
Han Kwan Saing <saihankwansaing@gmail.com>
2025-12-21 13:52:15 +0900

    try multithreading

--- javaStudy/basicJava/studyBasic/src/multithreading/Main.java
+++ javaStudy/basicJava/studyBasic/src/multithreading/Main.java
@@ -2,13 +2,24 @@ package multithreading;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         Thread thread1 = new Thread(new Myrunnable("hiii"));
         Thread thread2 = new Thread(new Myrunnable("hooo"));
 
+        System.out.println("start!");
+
         thread1.start();
         thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            System.out.println("main thread interrupted");
+        }
+
+        System.out.println("stopppp!");
 
     }
 
