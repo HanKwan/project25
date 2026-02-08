03f598251471162f094bbdf22fef0bc679259276
Han Kwan Saing <saihankwansaing@gmail.com>
2026-02-09 01:54:53 +0900

    use IOC container to let Spring do work

--- javaStudy/springBoot/first/src/main/java/com/firstIntoSP/first/FirstApplication.java
+++ javaStudy/springBoot/first/src/main/java/com/firstIntoSP/first/FirstApplication.java
@@ -2,15 +2,16 @@ package com.firstIntoSP.first;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 //@RestController
 public class FirstApplication {
 
 	public static void main(String[] args) {
-        var contact = SpringApplication.run(FirstApplication.class, args);
+        ApplicationContext context = SpringApplication.run(FirstApplication.class, args);
 
-        MyFirstService myFirstService = contact.getBean(MyFirstService.class);
+        var myFirstService = context.getBean(MyFirstService.class);
         System.out.println(myFirstService.useMethodFromMyFirstClass());
         System.out.println(myFirstService.getCustomProperty());
         System.out.println(myFirstService.getCustomInteger());
