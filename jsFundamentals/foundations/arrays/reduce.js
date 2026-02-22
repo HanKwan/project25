dc5ace8d591c47f289f08ee4997fd701d1d30f05
Han Kwan Saing <saihankwansaing@gmail.com>
2026-02-22 15:23:40 +0900

    get avg scores using normal and arrow function with reduce

--- jsFundamentals/foundations/arrays/reduce.js
+++ jsFundamentals/foundations/arrays/reduce.js
@@ -1,7 +1,22 @@
+// reduce return new value in one 
+
 const scores = [40, 32, 80, 95];
 
 const highScore = scores.reduce(getMaxScore);
-const avgScore = scores.reduce(getAvg, 0) / scores.length;
+const avgScore = scores.reduce(getAvg, 0) / scores.length; // divide the score.reduce
+
+// using arrow function
+const avgScoreWithArr = scores.reduce( (pre, next) => pre + next, 0) / scores.length;
+
+console.log(`avg score with arrow function ${avgScoreWithArr}`);
+
+// using normal function
+const avgScoreWithNorFun = scores.reduce(function(pre, next) {
+    return pre + next;
+}, 0) / scores.length;
+
+console.log(`avg score with normal function ${avgScoreWithNorFun}`);
+
 
 
 function getMaxScore(pre, next) {
