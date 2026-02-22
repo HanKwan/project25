c982540353b8d3a9a291689dc4edcc7487d5e96c
Han Kwan Saing <saihankwansaing@gmail.com>
2026-02-23 01:02:40 +0900

    add: 2 person objects

---
+++ jsFundamentals/foundations/arrays/destructure.js
@@ -0,0 +1,12 @@
+const person1 = {
+    firstName: "Hiroshi",
+    lastName: "Tanaka",
+    age: 34,
+    job: "Developer"
+}
+
+const person2 = {
+    firstName: "Yuki",
+    lastName: "Yamamoto",
+    age: 28,
+} 
