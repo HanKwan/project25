80f0726f87b55cd6417c755fe723274a0cdc6849
Han Kwan Saing <saihankwansaing@gmail.com>
2026-02-23 14:03:15 +0900

    add: btn for attacks and check win function

--- jsFundamentals/foundations/functions/closure.js
+++ jsFundamentals/foundations/functions/closure.js
@@ -1,24 +1,41 @@
 let attack = 0;
-let mana = 20;
-let enmyHp = 50;
+let mana = 30;
+let enmyHp = 100; 
+
+console.log(`Enemy hp is ${enmyHp}`);
+
+function checkWin() {
+    if (enmyHp <= 0) {
+        console.log("GAME OVER! YOU WIN");
+        return true;
+    }
+    return false;
+}
 
 function swordHit() {
-    attack += 5;
+    attack += 10;
     enmyHp -= attack;
-    console.log(`You dealt +5dmg with your sword`);
-    console.log(`Enemy health: ${enmyHp}hp`);
+    console.log(`You dealt +10dmg with your sword`);
+
+    if (enmyHp <= 0) {
+        console.log(`Enemy health: 0hp`);
+    } else {
+        console.log(`Enemy health: ${enmyHp}hp`);
+    }
+    checkWin();
 }
 
 function useFireBall() {
-    mana -= 7;
-    enmyHp -= 14;
-    console.log(`You used fire spell. Mana: -${mana}`);
+    if (mana <= 8) {
+        console.log("Noth enough mana!");
+        return;
+    }
+
+    mana -= 9;
+    enmyHp -= 20;
+    console.log(`You used -9mana to cast fire spell. Mana: ${mana}`);
     console.log(`You dealt +14dmg with magic`);
     console.log(`Enemy health: ${enmyHp}hp`);
+    checkWin();
 }
 
-swordHit()
-useFireBall()
-if (enmyHp <= 0) {
-    console.log("GAME OVER! YOU WIN");
-}
