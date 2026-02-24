f9529251ed659ae79bc1f09abb29ed7a446e61bc
Han Kwan Saing <saihankwansaing@gmail.com>
2026-02-24 13:09:02 +0900

    add: display clock with setInterval for military time

--- jsFundamentals/projects/digital clock/script.js
+++ jsFundamentals/projects/digital clock/script.js
@@ -1,8 +1,14 @@
 
 function display() {
     
-    
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, 0);
+    const mins = now.getMinutes().toString().padStart(2, 0);
+    const sec = now.getSeconds().toString().padStart(2, 0);
+    const theClock = `${hours}:${mins}:${sec}`
+    document.getElementById("clock").textContent = theClock;
 
 }
 
+setInterval(display);
 display();
