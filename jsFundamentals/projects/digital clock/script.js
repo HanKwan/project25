3483f9c6fa24b3592cbfcc87d8a2c5856304dc6f
Han Kwan Saing <saihankwansaing@gmail.com>
2026-02-24 13:14:01 +0900

    make: 12-hour time for the digital clock with am and pm

--- jsFundamentals/projects/digital clock/script.js
+++ jsFundamentals/projects/digital clock/script.js
@@ -2,10 +2,13 @@
 function display() {
     
     const now = new Date();
-    const hours = now.getHours().toString().padStart(2, 0);
+    let hours = now.getHours();
+    const amPm = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12 || 12;
+    hours = hours.toString().padStart(2, 0);
     const mins = now.getMinutes().toString().padStart(2, 0);
     const sec = now.getSeconds().toString().padStart(2, 0);
-    const theClock = `${hours}:${mins}:${sec}`
+    const theClock = `${hours}:${mins}:${sec} ${amPm}`
     document.getElementById("clock").textContent = theClock;
 
 }
