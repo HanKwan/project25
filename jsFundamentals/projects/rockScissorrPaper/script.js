66e5bde89a6e469ec720b3199c5e1a7d39d42bae
Han Kwan Saing <saihankwansaing@gmail.com>
2026-03-01 23:30:09 +0900

    add some notes for learning

--- jsFundamentals/projects/rockScissorrPaper/script.js
+++ jsFundamentals/projects/rockScissorrPaper/script.js
@@ -21,33 +21,37 @@ function playGame(playerChoice) {
     if (playerChoice === computerChoice) {
         result = "It's a TIE!";  // it's not resultDisplay.textContent, its result
 
-    } else if (winRules[playerChoice] === computerChoice) {
-        result = "You Win!";
-        playerWinCount++;
+    } else if (winRules[playerChoice] === computerChoice) {     //-> if player choose rock and computer choose scissor,
+        result = "You Win!";                                    // winRules[Rock] === Scissor
+        playerWinCount++;                                       // Scissor === Scissor -> return true
     } else {
         result = "You Lose!";
         computerWinCount++;
     }
 
-
+    // output display of choices and the result
     playerPick.textContent = `Player Choose: ${playerChoice}`;
     computerPick.textContent = `Computer Choose: ${computerChoice}`;
     resultDisplay.textContent = result;
     
+    // default class for result
     resultDisplay.classList.remove("winResult", "loseResult");
     
+    // win and lose classes for result
     if (result === "You Win!") {
         resultDisplay.classList.add("winResult");
     } else if (result === "You Lose!") {
         resultDisplay.classList.add("loseResult");
     }
 
+    // win count display for player and computer
     playerWin.textContent = playerWinCount;
     computerWin.textContent = computerWinCount;
 
     updateScore();
 }
 
+// function for win and lose classes for win count: higher score for green color
 function updateScore() {
 
     playerWin.classList.remove("winResult", "loseResult");
