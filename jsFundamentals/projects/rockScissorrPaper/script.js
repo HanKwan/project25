11dbad86160ce3e237b0d721b119d5813ec97a40
Han Kwan Saing <saihankwansaing@gmail.com>
2026-03-01 22:28:33 +0900

    add: win count with classList method

--- jsFundamentals/projects/rockScissorrPaper/script.js
+++ jsFundamentals/projects/rockScissorrPaper/script.js
@@ -1,13 +1,17 @@
 const resultDisplay = document.getElementById("result");
 const playerPick = document.getElementById("playerPick");
 const computerPick = document.getElementById("computerPick");
+const playerWin = document.getElementById("playerWin");
+const computerWin = document.getElementById("computerWin");
 const choices = ["Rock", "Paper", "Scissor"];
+let playerWinCount = 0;
+let computerWinCount = 0;
 
 function playGame(playerChoice) {
     
     let computerChoice = choices[Math.floor(Math.random() * 3)];
     let result = "";
-    
+
     if (playerChoice === computerChoice) {
         result = "It's a TIE!";  // it's not resultDisplay.textContent, its result
 
@@ -30,17 +34,36 @@ function playGame(playerChoice) {
     playerPick.textContent = `Player Choose: ${playerChoice}`;
     computerPick.textContent = `Computer Choose: ${computerChoice}`;
     resultDisplay.textContent = result;
-
+    
     resultDisplay.classList.remove("winResult", "loseResult");
-
+    
     switch (result) {
+
         case "You Win!":
             resultDisplay.classList.add("winResult");
+            playerWinCount++;
             break;
-        case "You Lose!":
-            resultDisplay.classList.add("loseResult");
-            break;
-        default:
-            break;
+
+            case "You Lose!":
+                resultDisplay.classList.add("loseResult");
+                computerWinCount++;
+                break;
+
+            default:
+                break;
+            }
+
+    playerWin.textContent = playerWinCount;
+    computerWin.textContent = computerWinCount;
+
+    if (playerWinCount > computerWinCount) {
+        playerWin.classList.add("winResult");
+        computerWin.classList.add("loseResult");
+    } else if (computerWinCount > playerWinCount) {
+        computerWin.classList.add("winResult");
+        playerWin.classList.add("loseResult");
+    } else {
+        playerWin.classList.remove("winResult", "loseResult");
+        computerWin.classList.remove("winResult", "loseResult");
     }
 }
